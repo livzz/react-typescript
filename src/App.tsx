5bb41a739751ef7aa1b37f4595741d1fe76c36df
Alibh <alibh619@gmail.com>
2019-12-06 08:56:53 +0530

    created a component with props validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Message from './Message'
 
 function App() {
   // tuple
-  let aTuple: [string, number] = ["hello", 35];
+  // let aTuple: [string, number] = ["hello", 35];
   // enum
-  enum codes {
-    first = 1,
-    second
-  }
+  // enum codes {
+  //   first = 1,
+  //   second
+  // }
 
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value {codes.first} is of {typeof codes} type!
-        </p>
+        <Message message="Urgent Message" />
       </header>
     </div>
   );
