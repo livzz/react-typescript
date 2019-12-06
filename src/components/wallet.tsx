76f184509c93063ee69f043b3b06c5c7fedc4134
Alibh <alibh619@gmail.com>
2019-12-06 22:32:33 +0530

    created wallet and added test

---
+++ src/components/wallet.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+interface Wallet {
+  balance: Number
+}
+
+export const Wallet = (props: Wallet) => {
+  return (<div>
+    <h3 className="balance">
+      Wallet balance: {props.balance}
+    </h3>
+  </div>)
+}
+
+const mapStatetoProps = (state: any) => {
+  return {
+    balance: state.loot.balance
+  }
+}
+
+
+export default connect(mapStatetoProps)(Wallet)
